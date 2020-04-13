ad4b995ce2d36c180dcb98875ca29242af0a375a
edCoder1 <edwin.lsb1@gmail.com>
2020-04-12 19:11:26 -0500

    exception filters correctly applied?

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AllExceptionsFilter } from './observer-pattern/exception_filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
--- src/observer-pattern/observer-pattern.module.ts
+++ src/observer-pattern/observer-pattern.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { ObserverService } from './observer/observer.service';
 import { ObservableService } from './observable/observable.service';
 import { ObserverPatternController } from './observer-pattern.controller';
+import { APP_FILTER } from '@nestjs/core/constants';
+import { HttpExceptionFilter } from './exception_filters/http-exeption.filter';
 // import { Observable } from './observable/Observable';
 // import { Observer } from './observer/Observer';
 
 @Module({
   // imports: [Observable, Observer],
   controllers: [ObserverPatternController],
-  providers: [ObserverService, ObservableService],
-  exports: [ObservableService],
+  providers: [
+    ObserverService,
+    ObservableService,
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
+  exports: [ObservableService], //    NO NEED TO EXPORT SERVICES AS THEY ARE USED IN THIS MODULE ONLY - I WAS WRONG, IT IS NEEDED!
 })
 export class ObserverPatternModule {}
